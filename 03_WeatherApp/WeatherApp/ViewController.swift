09214b5b5d962882e4b4dbf32ad0a40b67112449
Julia <berrylily.0709@gmail.com>
2017-09-21 14:08:26 -0700

    Implement Tabs

--- 03_WeatherApp/WeatherApp/ViewController.swift
+++ 03_WeatherApp/WeatherApp/ViewController.swift
@@ -12,14 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.orange
+        
+        view.backgroundColor = UIColor.blue
+        
+        let button = UIButton(frame: CGRect(x: view.bounds.width / 2 - 50, y: view.bounds.height / 2, width: 100, height: 50))
+        button.backgroundColor = UIColor.black
+        button.addTarget(self, action: #selector(pushToNextVC), for: .touchUpInside)
+        view.addSubview(button)
+        
     }
-
+    
+    func pushToNextVC(){
+        let newVC = UIViewController()
+        newVC.view.backgroundColor = UIColor.red
+        
+        self.navigationController?.pushViewController(newVC, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
