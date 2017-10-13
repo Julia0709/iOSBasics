a5f6aae466e2d894ef7a3c2623cf46aa68c34ac5
Julia <berrylily.0709@gmail.com>
2017-10-12 17:47:36 -0700

    Implement calclateTip

--- 05_TipCalculatorApp/TipCalculatorApp/ViewController.swift
+++ 05_TipCalculatorApp/TipCalculatorApp/ViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var calclateTipButton: UIButton!
+    @IBOutlet weak var tipAmountTextLabel: UILabel!
+    
+    @IBAction func calclateTip(_ sender: UIButton) {
+        let bill = Double(billAmountTextField.text!)!
+        tipAmountTextLabel.text = String(bill * 0.15)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        billAmountTextField.keyboardType = UIKeyboardType.numberPad
+        tipAmountTextLabel.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
