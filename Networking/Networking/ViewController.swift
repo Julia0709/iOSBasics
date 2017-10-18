b12ca6381f28333e89db1339a945fe53d65ccbd3
Julia <berrylily.0709@gmail.com>
2017-10-18 12:22:08 -0700

    Add imageView

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -9,34 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var rootView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let url = URL(string: "http://imgur.com/y9MIaCS.png") {
-//            imageView.contentMode = .scaleAspectFit
+            rootView.contentMode = .scaleAspectFit
             downloadImage(url: url)
         }
-        print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
+        print("Image shows up when finish downloading")
     }
-
+    
+    func downloadImage(url: URL) {
+        // Download start
+        getDataFromUrl(url: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Downloading error")
+                return
+            }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            // Display image on view
+            DispatchQueue.main.async() {
+                self.addImageView(data: data)
+            }
+        }
+    }
+    
     func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url) { data, response, error in
             completion(data, response, error)
             }.resume()
     }
     
-    func downloadImage(url: URL) {
-        // Download start
-        getDataFromUrl(url: url) { data, response, error in
-            guard let data = data, error == nil else { return }
-            print(response?.suggestedFilename ?? url.lastPathComponent)
-            // Display on view
-            DispatchQueue.main.async() {
-                print(data)
-//                self.imageView.image = UIImage(data: data)
-            }
-        }
+    func addImageView(data: Data) {
+        let imageView = UIImageView(image: UIImage(data: data))
+        imageView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: UIScreen.main.bounds.width,
+            height: UIScreen.main.bounds.height
+        )
+        rootView.addSubview(imageView)
     }
 }
 
