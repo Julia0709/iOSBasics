cbc5cd099beaf17362562e7b7a7a3b959f20b672
Julia <berrylily.0709@gmail.com>
2017-10-18 10:14:22 -0700

    Create Networking project

---
+++ Networking/Networking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Julia on 2017/10/18.
+//  Copyright © 2017 Julia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
