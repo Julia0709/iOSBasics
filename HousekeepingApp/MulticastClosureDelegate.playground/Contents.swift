ffd441cf85fa888c1eb0df97405d2c6642558d8b
Julia <berrylily.0709@gmail.com>
2017-11-20 14:15:01 -0800

    Implement Multclosure

--- HousekeepingApp/MulticastClosureDelegate.playground/Contents.swift
+++ HousekeepingApp/MulticastClosureDelegate.playground/Contents.swift
@@ -38,12 +38,58 @@ public class MulticastClosureDelegate<Success, Failure> {
   
   // MARK: - Callback
   class Callback {
+    let queue: DispatchQueue
+    let success: Success
+    let failure: Failure
     
+    init(queue: DispatchQueue, success: Success, failure: Failure) {
+      self.queue = queue
+      self.success = success
+      self.failure = failure
+    }
   }
   
   // MARK: - Instance Properties
+  fileprivate var mapTable = NSMapTable<AnyObject, NSMutableArray>.weakToStrongObjects()
+  
+  public var count: Int {
+    return getCallbacks(removeAfter: false).count
+  }
   
   // MARK: - Instance Methods
+  public func addClosurePair(for objectKey: AnyObject, queue: DispatchQueue = .main, success: Success, failure: Failure) {
+    let callback = Callback(queue: queue, success: success, failure: failure)
+    let array = mapTable.object(forKey: objectKey) ?? NSMutableArray()
+    array.add(callback)
+    mapTable.setObject(array, forKey: objectKey)
+  }
+  
+  public func getSuccessTuples(removeAfter: Bool = true) -> [(Success, DispatchQueue)] {
+    return getCallbacks(removeAfter: removeAfter).map({ (callback) in
+      return (callback.success, callback.queue)
+    })
+  }
+  
+  public func getFailureTuples(removeAfter: Bool = true) -> [(Failure, DispatchQueue)] {
+    return getCallbacks(removeAfter: removeAfter).map({ (callback) in
+      return (callback.failure, callback.queue)
+    })
+  }
+  
+  fileprivate func getCallbacks(removeAfter: Bool = true) -> [Callback] {
+    let objects = mapTable.keyEnumerator().allObjects as [AnyObject]
+    let callbacks: [Callback] = objects.reduce([]) { (combinedArray, objectKey) in
+      let array = mapTable.object(forKey: objectKey)! as! [Callback]
+      return combinedArray + array
+    }
+    
+    guard removeAfter else { return callbacks }
+    objects.forEach { (object) in
+      mapTable.removeObject(forKey: object)
+    }
+    
+    return callbacks
+  }
 }
 
 // MARK: - Testing
@@ -52,3 +98,35 @@ typealias Failure = () -> Void
 
 let multicastDelegate = MulticastClosureDelegate<Success, Failure>()
 let delegate = NSObject()
+
+multicastDelegate.addClosurePair(for: delegate, success: { print("GOOD") }, failure: { print("BAD") })
+
+print("Way1: ")
+var callback = multicastDelegate.mapTable.object(forKey: delegate)?.firstObject as! MulticastClosureDelegate<Success, Failure>.Callback
+callback.success()
+
+print("Way2: ")
+callback = multicastDelegate.getCallbacks(removeAfter: false).first!
+
+let (success, queue) = multicastDelegate.getSuccessTuples(removeAfter: false).first!
+success()
+
+print("MulticastDelegate count: \(multicastDelegate.count)")
+
+
+// MARK: - Multithreading
+//print("-- Multithreading --")
+//multicastDelegate.mapTable.removeAllObjects()
+//
+//let count = 3
+//for _ in 0 ..< 3 {
+//  DispatchQueue.global(qos: .background).async {
+//    multicastDelegate.addClosurePair(for: objectKey, success: {
+//
+//    }, failure: {
+//    })
+//    print("count: \(multicastDelegate.count)")
+//  }
+//}
+
+
