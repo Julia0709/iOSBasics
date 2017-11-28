2669671703d625e61dbf5e8638149d476f10dec8
Julia <berrylily.0709@gmail.com>
2017-11-27 17:39:12 -0800

    Add project

---
+++ 14_TodoAppUsingCoreData/NoSplit/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  NoSplit
+//
+//  Created by Derrick on 2017-11-27.
+//  Copyright © 2017 Derrick Park. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Todo {
+    let title: String
+    let todoDescription: String
+    let priority: Int
+    let isCompleted: Bool
+    
+}
